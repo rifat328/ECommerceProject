6431a07c89db48745efdbeda1cb34a6dbb28d2b2
Rifat <rifathossain328663@gmail.com>
2023-07-12 21:54:45 +0600

    user Entity DAL

--- DAL/Entities/User.cs
+++ DAL/Entities/User.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace ECommerceProject.DAL.Entities
 {
-    internal class User
+    public class User
     {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; } ////   - Password(hashed and salted)
+        public enum UserType { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+
+
     }
 }
