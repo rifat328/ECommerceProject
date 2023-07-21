c681658b5a8d72f2eccfc74ba3e4a5a2597b61c3
Rifat <rifathossain328663@gmail.com>
2023-07-21 09:33:55 +0600

    product Controller

---
+++ ECommerceProject.API/Controllers/ProductController.cs
@@ -0,0 +1,88 @@
+﻿using ECommerceProject.DAL.Entities;
+using ECommerceProject.DAL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [ResponseCache(Duration = 10)]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var Products = _productRepository.GetAll();
+            return Ok(Products);
+        }
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            var Products = _productRepository.GetProduct(id);
+            return Ok(Products);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Product product)
+        {
+            try
+            {
+                bool isSaved = _productRepository.Add(product);
+                if (isSaved)
+                {
+                    return Ok("Product has been saved.");
+                }
+                return BadRequest("Product saved failed.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Edit([FromBody] Product product)
+        {
+            try
+            {
+                bool isUpdated = _productRepository.Edit(product);
+                if (isUpdated)
+                {
+                    return Ok("Product has been modified.");
+                }
+                return BadRequest("Product modified failed.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                bool isDeleted = _productRepository.Delete(id);
+                if (isDeleted)
+                {
+                    return Ok("Product has been deleted.");
+                }
+                return BadRequest("Product deleted failed.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- ECommerceProject.API/Program.cs
+++ ECommerceProject.API/Program.cs
@@ -1,4 +1,6 @@
 using ECommerceProject.DAL.Data;
+using ECommerceProject.DAL.Interfaces;
+using ECommerceProject.DAL.Repositorys;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +11,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 //Dependeny Injection:   conn string = ECommerceAppString on appsettings.json
 builder.Services.AddDbContext<ECommerceDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ECommerceAppString")));
+
+//  Entity Per Service + Feture Related Service build, Add dependency injection per entity and features
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<ICartItemRepository, CartItemRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IWishlistRepository, WishlistRepository>();
+builder.Services.AddScoped<IWishlistItemRepository, WishlistItemRepository>();
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
