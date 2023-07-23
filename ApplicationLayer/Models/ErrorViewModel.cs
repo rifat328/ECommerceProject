a5f36033cbf15bc209fe4c602badf28015d2fe05
Rifat <rifathossain328663@gmail.com>
2023-07-23 06:18:07 +0600

    next change

--- ApplicationLayer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ApplicationLayer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
